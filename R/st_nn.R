51f23f08198f0c918f01ca9989b979ad33614162
Michael Dorman <dorman@post.bgu.ac.il>
2020-08-11 12:20:02 +0000

    version 0.3.9

--- R/st_nn.R
+++ R/st_nn.R
@@ -21,7 +21,7 @@
 #' \item{If \code{sparse=FALSE}, a \code{logical} matrix with element \code{[i,j]} being \code{TRUE} when \code{y[j,]} is a neighbor of \code{x[i]}.}
 #' \item{If \code{returnDists=TRUE} the function returns a \code{list}, with the first element as specified above, and the second element a sparse \code{list} with the distances (as \code{units} vectors, \strong{in meters}) between each pair of detected neighbors corresponding to the sparse \code{list} of indices.}
 #' }
-#' @references C. F. F. Karney, GeographicLib, Version 1.49 (2017-mm-dd), \url{https://geographiclib.sourceforge.io/1.49}
+#' @references C. F. F. Karney, GeographicLib, Version 1.49 (2017-mm-dd), \url{https://geographiclib.sourceforge.io/1.49/}
 #' @export
 #' @import sf
 #'
@@ -213,7 +213,7 @@ st_nn = function(x, y, sparse = TRUE, k = 1, maxdist = Inf, returnDist = FALSE, 
     ids = lapply(result, function(i) i[[1]])
     ids = do.call(c, ids)
     names(ids) = NULL
-    dists = lapply(result, function(i) i[[1]])
+    dists = lapply(result, function(i) i[[2]])
     dists = do.call(c, dists)
     names(dists) = NULL
   }
@@ -240,14 +240,4 @@ st_nn = function(x, y, sparse = TRUE, k = 1, maxdist = Inf, returnDist = FALSE, 
   return(result)
 
 }
-
-
-
-
-
-
-
-
-
-
 
